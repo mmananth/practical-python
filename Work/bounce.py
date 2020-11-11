193d90a0834cfb049586d5157e14020328cfbced
mmananth <ananth.mm@gmail.com>
2020-11-11 20:21:35 +0530

    Changes made for exercise solutions

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+
+def bounce(ht,bounce_back):
+    for i in range(10):
+        ht = round(ht*bounce_back,4)
+        print(f'{i+1} {ht}')
+        print('{0:} {1:}'.format(i+1,ht))
+
+if __name__ == '__main__':
+    bounce(100,3/5)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0
+with open('Data/portfolio.csv', 'rt') as f:
+    header = next(f)
+    for line in f:
+        stock_data = line.split(',')
+        total_cost = total_cost + int(stock_data[1]) * float(stock_data[2])
+
+print('Total cost ', str(total_cost))
