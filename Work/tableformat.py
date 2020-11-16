3eea142e425f87be6785e75b2950da90f936f419
mmananth <ananth.mm@gmail.com>
2020-11-16 18:45:16 +0530

    Changes made upto exercise 4.8

--- Work/tableformat.py
+++ Work/tableformat.py
@@ -10,3 +10,59 @@ class TableFormatter:
         Emit a single row of table data
         '''
         raise NotImplementedError()
+
+
+def create_formatter(name):
+    '''
+    Create output format for given input
+    '''
+    if name == 'txt':
+        return TextTableFormatter()
+    elif name == 'csv':
+        return CSVTableFormatter()
+    elif name == 'html':
+        return HTMLTableFormatter()
+    else:
+        raise RuntimeError(f'Unknown format {name}')
+        
+
+class TextTableFormatter(TableFormatter):
+    '''
+    Emit a table in plain-text format
+    '''
+    def headings(self, headers):
+        for h in headers:
+            print(f'{h:>10s}', end=' ')
+        print()
+        print(('-'*10 + ' ')*len(headers))
+
+    def row(self, rowdata):
+        for d in rowdata:
+            print(f'{d:>10s}', end=' ')
+        print()
+
+class CSVTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in CSV format
+    '''
+    def headings(self, headers):
+        print(','.join(headers))
+
+    def row(self, rowdata):
+        print(','.join(rowdata))
+
+class HTMLTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in HTML format
+    '''
+    def headings(self, headers):
+        print('<tr>', end='')
+        for h in headers:
+            print(f'<th>{h}</th>', end='')
+        print('</tr>')
+
+    def row(self, rowdata):
+        print('<tr>', end='')
+        for d in rowdata:
+            print(f'<td>{d}</td>', end='')
+        print('</tr>')
